f533395f7762eeddeb04c577c1385bf8f517a1bf
Bruno Solon <bruno_solon@hotmail.com>
2021-10-18 20:08:04 -0300

    added: react query

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
+import { Provider as ReduxProvider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './App';
 
 import { store } from './app/store';
 
+const queryClient = new QueryClient();
+
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <ReduxProvider store={store}>
+        <App />
+      </ReduxProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
